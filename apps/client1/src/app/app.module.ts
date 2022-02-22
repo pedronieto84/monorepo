ebc0aea725ea4f9ade47a5a5ade63d278f23ac0b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-22 18:38:30 +0100

    Importo el Login en el Modulo de Cliente 1

--- apps/client1/src/app/app.module.ts
+++ apps/client1/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { LoginModule } from '@pedro/login';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent],
 })
