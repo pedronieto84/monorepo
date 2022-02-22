8474e7488cd0ce611d3bd224eb72a99fcf2fc485
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-22 18:41:50 +0100

    Pongo el login en los clientes 2 y 3

--- apps/client2/src/app/app.module.ts
+++ apps/client2/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginModule } from '@pedro/login';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent],
 })
